dc7c85958cc6c47d96322e790a4834f36d8a75ce
Caryl Henry <carolynhenry2021@u.northwestern.edu>
2020-01-29 02:16:23 -0600

    First pass at part 2
    Currently still passes for part 1, but fails for part 2

--- streamer.py
+++ streamer.py
@@ -13,38 +13,59 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
-        self.seq_num = 0
-        self.rec_buf = []
+        self.expected_num = 0
+        self.rec_buf = {}
 
     def send(self, data_bytes: bytes):
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
         # length check should be dif bc must make space for header
-        header = bytes(b"".join([bytes(self.seq_num), b'\r\n\r\n']))
-        print('Header: ' + str(header))
-        payload = data_bytes
-        if len(data_bytes) > 1472-len(header):
+        print('sequence number: ' + str(self.expected_num))
+        header = str(self.expected_num).encode('utf-8')+b'\r\n\r\n'
+        if len(data_bytes) + len(header) > 1472:
             payload = data_bytes[:1472-len(header)]
-            remainder = data_bytes[1472:]
-            data_bytes = data_bytes[:1472]
-            packet = b"".join([header, payload])
-            self.seq_num += len(packet)
-            # for now I'm just sending the raw application-level data in one UDP payload
-            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-            self.send(remainder)
+            self.socket.sendto(header+payload, (self.dst_ip, self.dst_port))
+            self.expected_num += 1472
+            self.send(data_bytes[1472-len(header):])
         else:
-            self.send(b"".join([header, payload]))
+            self.socket.sendto(header+data_bytes, (self.dst_ip, self.dst_port))
+            self.expected_num += len(header)+len(data_bytes)
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
-        # get the data
-        # check its sequence number
-        # if there is a gap, do something
-        # if there is no gap, do something else
-        #
 
-        # this sample code just calls the recvfrom method on the LossySocket
         data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        print('data: ' + str(data))
+        print('data length: ' + str(len(data)))
+        seq_num = get_seq_num(data)
+        payload = get_payload(data)
+        # keep retrieving data until the expected sequence num comes
+        while seq_num != self.expected_num:
+            data, addr = self.socket.recvfrom()
+            print('expected sequence number: ' + str(self.expected_num))
+            print('data: ' + str(data))
+            print('data length: ' + str(len(data)))
+            seq_num = get_seq_num(data)
+            # if incoming data doesn't have sequence num, add it to the buffer
+            self.rec_buf[seq_num] = data
+        # increase the expected sequence num once the packet has come in
+        self.expected_num += len(data)
+        application_data = payload
+        while self.expected_num in self.rec_buf:
+            application_data += get_payload(self.rec_buf[self.expected_num])
+            new_expected_num = self.expected_num + \
+                len(self.rec_buf[self.expected_num])
+            self.rec_buf.pop(self.expected_num)
+            self.expected_num = new_expected_num
+        return application_data
+
+
+def get_seq_num(data: bytes):
+    num = data.decode('utf-8')
+    return int(num[:num.find('\r\n\r\n')])
+
+
+def get_payload(data: bytes):
+    data = data.decode('utf-8')
+    return data[data.find('\r\n\r\n')+len('\r\n\r\n'):].encode('utf-8')
