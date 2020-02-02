feef4b53d80637aa83adb27f7c56a78b421508b5
Caryl Henry <carolynhenry2021@u.northwestern.edu>
2020-02-02 15:29:04 -0600

    SUPER slow implementation that probably works (without closing still)

--- streamer.py
+++ streamer.py
@@ -2,6 +2,9 @@
 from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
+# threading for part 3: packet loss
+from concurrent.futures import ThreadPoolExecutor
+from threading import Timer
 
 
 class Streamer:
@@ -15,92 +18,110 @@ class Streamer:
         self.dst_port = dst_port
         self.expected_num = 0
         self.rec_buf = {}
+        self.timer_buf = {}
+        self.listener = True
+        self.timeout = .25
+        self.executor = ThreadPoolExecutor(max_workers=5)
+        self.future = self.executor.submit(self.listening)
+        self.received_acks = set([])
+        self.sent_acks = set([])
+
+    def listening(self) -> bytes:
+        """Blocks (waits) if no data is ready to be read from the connection."""
+        while self.listener:
+            data, addr = self.socket.recvfrom()
+            print('raw data: ' + str(data))
+            print('expected num: ' + str(self.expected_num))
+            print(self.rec_buf.keys())
+            if data and data[0] == 65:
+                self.ack_recv(data)
+            elif data:
+                seq_num = get_seq_num(data)
+                if seq_num >= self.expected_num:
+                    self.rec_buf[seq_num] = data
+                self.ack_send(data)
 
     def send(self, data_bytes: bytes):
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
         # length check should be dif bc must make space for header
-        seq_num = str(self.expected_num).encode('utf-8')
-        if len(data_bytes) + len(seq_num) + 6 + len(b'\r\n\r\n') > 1472:
-            payload = data_bytes[:1472-len(seq_num)-6-len(b'\r\n\r\n')]
-            checksum = self.calculate_checksum(payload).encode('utf-8')
-            print('send checksum: ' + checksum.decode('utf-8'))
-            header = seq_num+b'C'+checksum+b'\r\n\r\n'
-            print(len(header+payload))
+        header = str(self.expected_num).encode('utf-8')+b'\r\n\r\n'
+        if len(data_bytes) + len(header) > 1472:
+            payload = data_bytes[:1472-len(header)]
             self.socket.sendto(header+payload, (self.dst_ip, self.dst_port))
-            self.expected_num += 1472
+            self.expected_num += len(header+payload)
+            timer = Timer(self.timeout, self.retransmission, [header+payload])
+
+            self.timer_buf[self.expected_num] = timer
+
+            timer.start()
             self.send(data_bytes[1472-len(header):])
         else:
-            checksum = self.calculate_checksum(data_bytes).encode('utf-8')
-            header = seq_num+b'C'+checksum+b'\r\n\r\n'
             self.socket.sendto(header+data_bytes, (self.dst_ip, self.dst_port))
-            self.expected_num += len(header)+len(data_bytes)
+            self.expected_num += len(header+data_bytes)
+            timer = Timer(self.timeout, self.retransmission,
+                          [header+data_bytes])
+
+            self.timer_buf[self.expected_num] = timer
+
+            timer.start()
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-
-        data, addr = self.socket.recvfrom()
-        seq_num = get_seq_num(data)
-        checksum = get_checksum(data).decode('utf-8')
-        print('checksum field for receive: ' + checksum)
-        calculated_checksum = self.calculate_checksum(get_payload(data))
-        print('calculated checksum for receive: ' + calculated_checksum)
-        if checksum != self.calculate_checksum(get_payload(data)):
-            print('corrupted segment')
-            return b''
-        # if not expected data, add it to the buffer and return nothing
-        if seq_num != self.expected_num:
-            self.rec_buf[seq_num] = data
+        # if not expected data, return nothing
+        if self.expected_num not in self.rec_buf:
             return b''
 
         # if expected data, check buffer for contiguous segments and return total contiguous payload
-        self.expected_num += len(data)
-        application_data = get_payload(data)
+        application_data = self.rec_buf[self.expected_num]
+        self.rec_buf.pop(self.expected_num)
+        self.expected_num += len(application_data)
+        application_data = get_payload(application_data)
         while self.expected_num in self.rec_buf:
-            application_data += get_payload(self.rec_buf[self.expected_num])
-            new_expected_num = self.expected_num + \
-                len(self.rec_buf[self.expected_num])
+            data = self.rec_buf[self.expected_num]
             self.rec_buf.pop(self.expected_num)
-            self.expected_num = new_expected_num
+            self.expected_num += len(data)
+            application_data += get_payload(data)
         return application_data
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
-        # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        while len(self.timer_buf) != 0:
+            print('Waiting Timer buf: ' + str(self.timer_buf))
+            pass
+        self.listener = False
 
-    def calculate_checksum(self, msg):
-        msg = msg.decode('utf-8')
-        print('payload to calculate checksum: ' + msg)
-        s = 0       # Binary Sum
-        # loop taking 2 characters at a time
-        for i in range(0, len(msg), 2):
-            if (i+1) < len(msg):
-                a = ord(msg[i])
-                b = ord(msg[i+1])
-                s = s + (a+(b << 8))
-            elif (i+1) == len(msg):
-                s += ord(msg[i])
-            else:
-                raise "Something Wrong here"
-        # One's Complement
-        s = s + (s >> 16)
-        s = ~s & 0xffff
-        return str(s)
+    def ack_recv(self, data: bytes):
+        ack_num = get_ack_num(data)
+        self.received_acks.add(ack_num)
+        if ack_num in self.timer_buf.keys():
+            self.timer_buf[ack_num].cancel()
+            self.timer_buf.pop(ack_num)
+
+    def ack_send(self, data: bytes):
+        ack_num = str(get_seq_num(data)+len(data))
+        self.sent_acks.add(int(ack_num))
+        header = b'A'+ack_num.encode('utf-8')+b'\r\n\r\n'
+        self.socket.sendto(header, (self.dst_ip, self.dst_port))
+
+    def retransmission(self, data: bytes):
+        self.socket.sendto(data, (self.dst_ip, self.dst_port))
+        timer = Timer(self.timeout, self.retransmission, [data])
+        self.timer_buf[get_seq_num(data)+len(data)] = timer
+        timer.start()
 
 
-def get_seq_num(data: bytes):
+def get_seq_num(data: bytes) -> int:
     num = data.decode('utf-8')
-    return int(num[:num.find('C')])
+    return int(num[:num.find('\r\n\r\n')])
 
 
-def get_payload(data: bytes):
+def get_ack_num(data: bytes) -> int:
+    num = data.decode('utf-8')
+    ack_num = num[1:num.find('\r\n\r\n')]
+    return int(ack_num)
+
+
+def get_payload(data: bytes) -> bytes:
     data = data.decode('utf-8')
     return data[data.find('\r\n\r\n')+len('\r\n\r\n'):].encode('utf-8')
-
-
-def get_checksum(data: bytes):
-    data = data.decode('utf-8')
-    return data[data.find('C')+1:data.find('\r\n\r\n')].encode('utf-8')
